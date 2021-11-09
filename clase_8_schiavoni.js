f57d19e5343e5fd2c96bbc6094d828e97294182d
Franco Schiavoni <francoschiavoni@hotmail.com>
2021-11-08 21:06:04 -0300

    Se sacan todos los promt y se utiliza Forms y Buttons. Se incluye jQuery

--- clase_8_schiavoni.js
+++ clase_8_schiavoni.js
@@ -1,108 +1,56 @@
+// Corrección con FORM
+let sumaAlApretarBoton = 0;
+let presupuestoTotal = 0;
+const presupuestos = [];
+class Presupuesto{
+    constructor (pozo,valor){
+        this.pozo = pozo;
+        this.valor = parseInt(valor);
+    }     
+}
 
-
-
-// -----------------EVENTOS-----------------------------
-
-    let botonIngresar = document.getElementById ("botonIngresar");
-    botonIngresar.addEventListener ("click", respuestaClickAgregar);
-    let presupuestoTotal = 0;
-
-    function respuestaClickAgregar (e) {
-
-        e.preventDefault();
-       
-        cantPozos = prompt ("Ingrese la cantidad de pozos a cargar: ");
-        if(cantPozos == ""){
-            cantPozos=prompt("No ha ingresado ninguna cantidad. Por favor, Ingrese la nueva cantidad de pozos a cargar: ");    
-        }
-
-        class Presupuesto{
-            constructor (pozo,valor){
-                this.pozo = pozo;
-                this.valor = parseInt(valor);
-            }
-
-            
-        }
-
-        const presupuestos = [];
-
-        for (let i =1; i<= cantPozos; i++){
-            presupuestos.push (new Presupuesto (prompt("Nombre del pozo: "), parseInt(prompt("Ingrese valor del pozo: "))));
-        }
-        // ---------USO DE STORAGE Y JASON
-        //-----Para almacenar array completo:
-        const guardarLocalValores = (clave, valor) => {localStorage.setItem(clave,valor)};
-        //guardarLocal("todosLosPozos", JSON.stringify(presupuestos));
-
-         //---------USO DE STORAGE Y JASON
-          for (const presupuestoIndividual of presupuestos) {
-              guardarLocalValores (presupuestoIndividual.pozo, JSON.stringify(presupuestoIndividual.valor));
-          }
-        
-
-
-
-
-
-        // for(let elementodelarray of presupuestos){
-        //     presupuestoTotal = presupuestoTotal + elementodelarray.valor;
-        //     }
- 
-
-        //   let padre  = document.getElementById ("h3DePozos");
-        //   for(let elementodelarray of presupuestos){
-        //       let cadaPresupuesto = document.createElement("h3")
-        //       cadaPresupuesto.innerHTML =  "Pozo:  " + elementodelarray.pozo + "; " + "Valor: USD "+ elementodelarray.valor; 
-        //       padre.appendChild (cadaPresupuesto);
-        //       // document.body.appendChild (cadaPresupuesto);
-        //       }
+$("form").submit (function(e){
   
-        // let tituloPrincipalIndex = document.getElementById ("tituloPrincipalIndex");
-        // tituloPrincipalIndex.innerHTML = "El presupuesto total ingresado es de: USD " + presupuestoTotal;  
+    e.preventDefault();
+        presupuestos.push (new Presupuesto ($("#nombrePozo").val(), $("#precioPozo").val()));
+
+        const guardarLocalValores = (clave, valor) => {localStorage.setItem(clave,valor)};
+        guardarLocalValores ($("#nombrePozo").val(), $("#precioPozo").val());
+
+        $("#h3DePozos").append(`<h3>Pozo: ${$("#nombrePozo").val()}; Valor: USD ${$("#precioPozo").val()}</h3>`);
+       
+}) 
 
 
+         // ------------------------------BOTON MOSTRAR PRESUPUESTO CON JQUERY-------------
 
-        // for (let valores of presupuestos){
-        //     console.log(valores);
-        // }
-        }
-
-         // ------------------------------BOTON MOSTRAR PRESUPUESTO-------------
-
-         let botonPresupuestar = document.getElementById ("botonMostrarPresupuesto");
-         botonPresupuestar.addEventListener ("click", respuestaClickPresupuestar);
-         let padre  = document.getElementById ("h3DePozos");
-
-         function respuestaClickPresupuestar (e){
+         $("#botonMostrarPresupuesto").click (function(e){
              e.preventDefault();
              let sumaAlApretarBoton = 0;
-           
+             console.log (typeof sumaAlApretarBoton);
              for (let k=0 ; k < localStorage.length; k++){
     
-                 let claveJson =localStorage.key(k);
-                 let claveParseada = JSON.parse(localStorage.getItem(claveJson));
-                 let cadaPresupuesto = document.createElement("h3")
-                 cadaPresupuesto.innerHTML = "Pozo:  " + claveJson + "; " + "Valor: USD "+ claveParseada; 
-                 padre.appendChild (cadaPresupuesto);
+                let claveJson =localStorage.key(k);
+                console.log(typeof claveJson );
+                let claveParseada = JSON.parse(localStorage.getItem(claveJson));
+                console.log(typeof claveParseada );
 
-             sumaAlApretarBoton = sumaAlApretarBoton  + claveParseada; 
-             }
-             let tituloPrincipalIndex = document.getElementById ("tituloPrincipalIndex");
-             tituloPrincipalIndex.innerHTML = "El presupuesto total ingresado es de: USD " + sumaAlApretarBoton;  
+                sumaAlApretarBoton = sumaAlApretarBoton  + claveParseada; 
+                console.log(typeof sumaAlApretarBoton );
+            }
+            $("#tituloPrincipalIndex").html(`El presupuesto total ingresado es de: USD ${sumaAlApretarBoton}`);
+         })
+     
+       
 
-         }
 
-         
-        // -------------------------------BOTON BORRAR-------------
+// -------------------------------BOTON BORRAR CON JQUERY-------------
+$("#botonBorrar").click (function(e){
+    e.preventDefault();
+    $("#tituloPrincipalIndex").html(`Gracias por utilizar el servicio.`);
+    $("#h3DePozos").empty();
+    localStorage.clear();
+})
 
-        let botonBorrar = document.getElementById ("botonBorrar");
-        botonBorrar.addEventListener ("click", respuestaClickBorrar);
 
-        function respuestaClickBorrar (e){
-            e.preventDefault();
-           tituloPrincipalIndex.innerHTML = "Gracias por utilizar el servicio. ";
-           let listaEliminar = document.getElementById ("padreCronograma");
-           listaEliminar.parentNode.removeChild (listaEliminar);
-           localStorage.clear();
-        }
+
